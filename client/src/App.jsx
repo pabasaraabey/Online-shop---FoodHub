2e2c51eeca8ef0fe015fc8fe45937e9046a671c0
pabasara <pabasaraabeysinghe@gmail.com>
2025-04-09 21:37:49 +0530

    Updated App.jsx and added Categories, MainBanner, and new pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes,useLocation } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
+
+  const isSellerpath = useLocation().pathname.includes('/seller')
   return (
     <div>
-      <Navbar />
+
+      {isSellerpath ? null :<Navbar />}
+      <div className={'${isSellerpath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}'}>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
